b8c748786dd6e2c45755a85f1c59a5c7444c3d01
JimakPasha <jimak-pg@yandex.by>
2026-05-22 17:48:55 +0200

    test

---
+++ src/main.tsx
@@ -0,0 +1,71 @@
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+<div>
+  <div>LOGO</div>
+  <div>ATTEMPT</div>
+  <div>ATTEMPT</div>
+  <div>ATTEMPT</div>
+  <div>ATTEMPT</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>ATTEMPT 100500 + 100500</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</div>
+</div>
+)
